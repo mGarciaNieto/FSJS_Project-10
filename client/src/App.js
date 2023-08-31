392e80c88ec168e7266a412820f65f97e3e93b6f
Manuel Garcia-Nieto <mgarcianieto@gmail.com>
2023-09-01 01:02:13 +0200

    Add step 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+
+// function App() {
+// 	return (
+// 		<div className='App'>
+// 			<header className='App-header'>
+// 				<img src={logo} className='App-logo' alt='logo' />
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to reload.
+// 				</p>
+// 				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+// 					Learn React
+// 				</a>
+// 			</header>
+//       <section>
+//         <h1>Section 1</h1>
+//       </section>
+// 		</div>
+// 	)
+// }
+
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [courses, setCourses] = useState([])
+
+	useEffect(() => {
+		// Fetch the list of courses from the REST API
+		fetch('http://localhost:5000/api/courses') // Adjust the endpoint if it's different
+			.then((response) => response.json())
+			.then((data) => setCourses(data))
+			.catch((error) => console.error('Error fetching courses:', error))
+	}, [])
+
+	return (
+		<div className='App'>
+			<h1>Courses</h1>
+			<ul>
+				{courses.map((course) => (
+					<li key={course.id}>{course.title}</li> // Adjust based on your course object structure
+				))}
+			</ul>
+		</div>
+	)
 }
 
-export default App;
+export default App
