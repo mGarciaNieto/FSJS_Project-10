001c4ba5d994e02d659593485d4bc822e7cd5df5
Manuel Garcia-Nieto <mgarcianieto@gmail.com>
2023-09-23 23:39:48 +0200

    Add Courses and Header components

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+	return (
+		<header>
+			<div className='wrap header--flex'>
+				<h1 className='header--logo'>Courses</h1>
+				<nav>
+					<ul className='header--signedout'>
+						<li>Sign Up</li>
+						<li>Sign In</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	)
+}
